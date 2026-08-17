081455a58e4bb3223d48278dd551bf81a7973429
Michael VanderLoon <49925095+TNT928@users.noreply.github.com>
2026-08-17 15:50:35 -0400

    Add Long Key video poster image

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -16,6 +16,7 @@ const Home = () => {
     loop
     playsInline
     controls={true}
+    poster="https://vanderloonmedia.b-cdn.net/Thumbnails/Screenshot%202026-08-17%20at%203.44.53%E2%80%AFPM.png"
   >
     <source
       src="https://vanderloonmedia.b-cdn.net/Long%20Key%20Nature%20Center.mp4"
